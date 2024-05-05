17a68cb54bd0f76e55f43afeb526d37b08e7dea6
Collin Jilbert <cjilbert504@gmail.com>
2024-05-05 18:03:18 -0500

    Update RoadToElDurationTest to include tests for updating parent record duration_column when child records are added or removed and for using the duration_coder to serialize and deserialize the duration column

--- test/road_to_el_duration_test.rb
+++ test/road_to_el_duration_test.rb
@@ -4,4 +4,21 @@ class RoadToElDurationTest < ActiveSupport::TestCase
   test "it has a version number" do
     assert RoadToElDuration::VERSION
   end
+
+  test "it updates the parent record duration_column when child records are added or removed" do
+    episode = Episode.create(name: "Episode 1", duration: 10.minutes)
+    series = Series.create(name: "Series 1")
+    episode.update(series: series)
+
+    assert_equal 10.minutes, series.reload.duration
+  end
+
+  test "the duration_coder is used to serialize and deserialize the duration column" do
+    episode = Episode.create(name: "Episode 1", duration: 10.minutes)
+    series = Series.create(name: "Series 1")
+    episode.update(series: series)
+
+    binding.irb
+    assert_instance_of ActiveSupport::Duration, series.reload.duration
+  end
 end
