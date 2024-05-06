4eff2c09639bef0aaed18223437d10a54fd71aa3
Collin Jilbert <cjilbert504@gmail.com>
2024-05-06 06:18:23 -0500

    add test for transferring an episode to a different series

--- test/road_to_el_duration_test.rb
+++ test/road_to_el_duration_test.rb
@@ -1,16 +1,33 @@
 require "test_helper"
 
 class RoadToElDurationTest < ActiveSupport::TestCase
+  setup do
+    # @episode = episodes(:one)
+    # @series = series(:ruby_strings)
+    @episode_one = Episode.create(name: "Episode 1", duration: 10.minutes)
+    @series_one = Series.create(name: "Series 1")
+  end
+
   test "it has a version number" do
     assert RoadToElDuration::VERSION
   end
 
   test "it updates the parent record duration_column when child records are added or removed" do
-    episode = Episode.create(name: "Episode 1", duration: 10.minutes)
-    series = Series.create(name: "Series 1")
-    episode.update(series: series)
+    @episode_one.update(series: @series_one)
+    assert_equal 10.minutes, @series_one.reload.duration
 
-    assert_equal 10.minutes, series.reload.duration
+    @episode_one.update(series: nil)
+    assert_equal 0.minutes, @series_one.reload.duration
+  end
+
+  test "when moving a child record from one parent to another both parents are updated accordingly" do
+    @episode_one.update(series: @series_one)
+
+    @series_two = Series.create(name: "Series 2")
+
+    @episode_one.update(series: @series_two)
+    assert_equal 0.minutes, @series_one.reload.duration
+    assert_equal 10.minutes, @series_two.reload.duration
   end
 
   test "the duration_coder is used to serialize and deserialize the duration column" do
